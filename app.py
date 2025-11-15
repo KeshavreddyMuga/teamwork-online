bf1b1e8d5db532a5f88198791f476f68c217471f
KeshavreddyMuga <keshavareddymuga@gmail.com>
2025-11-16 00:57:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -357,4 +357,5 @@ def project_completed(pid):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, host="0.0.0.0", port=5000)
+        
+    socketio.run(app)
